03de5c2e1dee9e87a0fb114ee441f1e5ac5c7a51
José Valladares <32801195+TheAlexBig@users.noreply.github.com>
2024-12-06 00:56:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.FRONTEND_PORT || 3000;
 // Serve static files from the 'dist' directory (where your bundle will be built)
 app.use(express.static(path.join(__dirname, 'dist')));
 
